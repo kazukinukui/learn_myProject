5934fd68a5ad7735b330f713dee681454a6bf9a6
Nukui <kazuki.nukui@g.dreamcareer.co.jp>
2024-05-26 10:23:58 +0900

    hogehoge追加

--- src/main/java/com/example/bbbbbb/UserForm.java
+++ src/main/java/com/example/bbbbbb/UserForm.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 
 public class UserForm {
     
-    @NotBlank(message = "名前を入力してください(other1です)")
+    @NotBlank(message = "名前を入力してください(other1ですhogehoge)")
     private String name;
 
     public String getName() {

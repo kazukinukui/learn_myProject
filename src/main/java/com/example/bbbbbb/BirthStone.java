5e176e1a7c2ef3167fa2401111603386a6a5bb8d
Nukui <kazuki.nukui@g.dreamcareer.co.jp>
2024-05-26 22:38:34 +0900

    react連携用

---
+++ src/main/java/com/example/bbbbbb/BirthStone.java
@@ -0,0 +1,28 @@
+package com.example.bbbbbb;
+
+public class BirthStone {
+    private String stone;
+    private String month;
+
+    public BirthStone(String stone, String month) {
+        this.stone = stone;
+        this.month = month;
+    }
+
+    // getters and setters
+    public String getStone() {
+        return stone;
+    }
+
+    public void setStone(String stone) {
+        this.stone = stone;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+}
